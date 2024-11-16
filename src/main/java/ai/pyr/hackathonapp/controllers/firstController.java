b1915155e24bfd531d711186e78530a15fd53a32
Agata <buchwaldagata@gmail.com>
2024-11-16 14:16:52 +0100

    first enpoint get

---
+++ src/main/java/ai/pyr/hackathonapp/controllers/firstController.java
@@ -0,0 +1,14 @@
+package ai.pyr.hackathonapp.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("first-web")
+public class firstController {
+
+
+//    @GetMapping("/my_name")
+//    public String sayHello() {
+//        return "Hello, World 2!";
+//    }
+}
