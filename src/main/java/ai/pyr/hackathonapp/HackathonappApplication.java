b19c2edb5d2658d7fb0e4a6c7c8726dfc4943592
Mariusz Hybiak <mariusz.hybiak@student.put.poznan.pl>
2024-11-16 13:37:18 +0100

    Upload project

---
+++ src/main/java/ai/pyr/hackathonapp/HackathonappApplication.java
@@ -0,0 +1,13 @@
+package ai.pyr.hackathonapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ai/pyr/hackathonapp/HackathonappApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.pyr.hackathonapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathonappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
